f6dde5e4e098b75a04394eb6b659be07c42cc4d2
CarinaWollendorfer <30310907+CarinaWolli@users.noreply.github.com>
2021-12-18 03:52:22 -0500

    fix bugs related to target and source value

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,12 +1,13 @@
 import prisma from "../../lib/prisma";
 
 export default async function handle(req, res) {
-  const { fromUserId, toUserId, Sourcevalue, sourceCurrency, targetCurrency } = req.body;
+  const { fromUserId, toUserId, sourceValue, sourceCurrency, targetCurrency } = req.body;
   const response = await prisma.transaction.create({
     data: {
       fromUserId,
       toUserId,
-      Sourcevalue,
+      sourceValue, 
+      targetValue: 0.00,
       sourceCurrency,
       targetCurrency
     },
